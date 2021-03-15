1d886786c0dc893a92dcb731bdff206a43a958ff
Matthew Magee <matthewmagee17@gmail.com>
2021-03-15 18:45:19 +0000

    Reset password functionality via email confirmation

--- LisgreyWebApp_FYP/urls.py
+++ LisgreyWebApp_FYP/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django_email_verification import urls as email_urls
-
+from django.contrib.auth import views as auth_views
 from LisgreyWebApp import views
 from LisgreyWebApp_FYP import settings
 
@@ -32,8 +32,20 @@ urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
     path('gallery/', views.gallery_view, name='gallery'),
     path('account/register/', views.register, name='register'),
-    path('account/', include('django.contrib.auth.urls')),
-
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="registration/reset_password.html"),
+         name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="registration"
+                                                                                        "/password_reset_sent.html"),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_form.html"),
+         name='password_reset_confirm'),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_done.html"),
+         name='password_reset_complete'),
+    # path('accounts/', include('django.contrib.auth.urls')),
     path('', include('food_menus.urls')),
     path('reservation/', include('reservations.urls')),
     path('takeaway/', include('takeaway.urls')),
