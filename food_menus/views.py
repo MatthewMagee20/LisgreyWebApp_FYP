a2c910fe0829d1290d3469b8ab9c89355ebc4304
Matthew Magee <matthewmagee17@gmail.com>
2021-03-25 22:49:21 +0000

    commiting service worker

--- food_menus/views.py
+++ food_menus/views.py
@@ -5,23 +5,20 @@ from .models import FoodItem
 
 def get_food_menu_takeaway(request):
 
-    # starter_items = FoodItem.objects.filter(category="Starter")
-    # main_items = FoodItem.objects.filter(category="Main Course")
-    # kids_menu_items = FoodItem.objects.filter(category="Kiddie Menu")
-    # side_orders = FoodItem.objects.filter(category="Side Order")
-    # dessert_items = FoodItem.objects.filter(category="Dessert")
-    # drink_items = FoodItem.objects.filter(category="Drinks")
-
-    food_items = FoodItem.objects.all()
+    starter_items = FoodItem.objects.filter(category="Starter")
+    main_items = FoodItem.objects.filter(category="Main Course")
+    kids_menu_items = FoodItem.objects.filter(category="Kiddie Menu")
+    side_orders = FoodItem.objects.filter(category="Side Order")
+    dessert_items = FoodItem.objects.filter(category="Dessert")
+    drink_items = FoodItem.objects.filter(category="Drinks")
 
     data = {
-        # 'starters': starter_items,
-        # 'mains': main_items,
-        # 'kids_items': kids_menu_items,
-        # 'side_orders': side_orders,
-        # 'desserts': dessert_items,
-        # 'drinks': drink_items,
-        'items': food_items,
+        'starters': starter_items,
+        'mains': main_items,
+        'kids_items': kids_menu_items,
+        'side_orders': side_orders,
+        'desserts': dessert_items,
+        'drinks': drink_items,
         'allergens': FoodItem.ALLERGENS
     }
 
---
+++ static/js/serviceworker.js
@@ -0,0 +1,62 @@
+const filesToCache = [
+    '/',
+
+];
+
+const staticCacheName = 'pages-cache-v1';
+
+self.addEventListener('install', event => {
+    console.log('Attempting to install service worker and cache static assets');
+    event.waitUntil(
+        caches.open(staticCacheName)
+            .then(cache => {
+                return cache.addAll(filesToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url);
+    if ( event.request.url.indexOf( '/takeaway/basket/' ) !== -1 ) { // ignore the caching of basket session variable
+        return false;
+    }if ( event.request.url.indexOf( '/contact/' ) !== -1 ) { // ignore the caching of basket session variable
+        return false;
+    }
+    if ( event.request.url.indexOf( '/reservation/' ) !== -1 ) { // ignore the caching of basket session variable
+        return false;
+    }
+    if ( event.request.url.indexOf( '/account/register/' ) !== -1 ) { // ignore the caching of basket session variable
+        return false;
+    }
+    if ( event.request.url.indexOf( '/admin/' ) !== -1 ) { // ignore the caching of basket session variable
+        return false;
+    }
+
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    console.log('Found ', event.request.url, ' in cache');
+                    return response;
+                }
+                console.log('Network request for ', event.request.url);
+                return fetch(event.request)
+                    .then(response => {
+                        if (response.status === 404) {
+                            return caches.match('pages/404.html');
+                        }
+                        return caches.open(staticCacheName)
+                            .then(cache => {
+                                cache.put(event.request.url, response.clone());
+                                return response;
+                            });
+                    });
+            }).catch(error => {
+            console.log('Error, ', error);
+            return caches.match('pages/offline.html');
+        })
+    );
+});
+
+
+
